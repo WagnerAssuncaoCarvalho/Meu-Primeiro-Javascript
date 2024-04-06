9453bef90211b6b5b28a97d4f701e337213813da
Wagner Assunção Carvalho <151686000+WagnerAssuncaoCarvalho@users.noreply.github.com>
2024-04-06 17:44:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,83 @@
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento === null) {
+        console.log('Elemento não encontrado');
+    }
+    
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+
+    else {
+        console.log('Parâmetro inválido');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador =0; contador < listaDeTeclas.length;contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function (evento) {
+        
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+    }
+
+}
+
+    tecla.onkeyup = function (evento) {
+
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.remove('ativa');
+    }
+        
+}
+
+}
+
+/* Com While
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    contador = contador + 1;
+
+    //console.log(contador)
+
+}
+
+*/
+
+
+
+/* Com repetição
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();    
@@ -34,7 +111,6 @@ function tocaSomTom() {
     document.querySelector('#som_tecla_tom').play()
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 document.querySelector('.tecla_tim').onclick = tocaSomTim;
 document.querySelector('.tecla_puff').onclick = tocaSomPuff;
@@ -43,3 +119,4 @@ document.querySelector('.tecla_toim').onclick = tocaSomToim;
 document.querySelector('.tecla_psh').onclick = tocaSomPsh;
 document.querySelector('.tecla_tic').onclick = tocaSomTic;
 document.querySelector('.tecla_tom').onclick = tocaSomTom;
+*/
